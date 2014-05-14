832548ef875632da776b312d29c71f7305494b7e
Bohdon Sayre <bohdon@gmail.com>
2014-05-14 13:18:53 -0500

    reorganizes project into src folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,26 +2,40 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('./src/package.json'),
     sass: {
       dist: {
         files: {
-          'css/style.css': 'sass/style.scss'
+          'src/css/style.css': 'sass/style.scss'
         }
       }
     },
     jshint: {
       all: {
         src: [
-          'js/iosketch.js',
-          'js/wacom.js',
-          'js/sketchtest.js'
+          'src/js/iosketch.js',
+          'src/js/wacom.js',
+          'src/js/sketchtest.js'
         ]
       }
-    }
+    },
+    nodewebkit: {
+      options: {
+          build_dir: './webkitbuilds',
+          mac: true,
+          win: true,
+          linux32: false,
+          linux64: false
+      },
+      src: [
+        './src/**/*',
+        './node_modules/**/*'
+      ]
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-node-webkit-builder');
 
 };
