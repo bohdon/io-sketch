0fd05e29d89216c7534c2e7b654fc90c57de2dee
Bohdon Sayre <bohdon@gmail.com>
2014-05-21 10:05:21 -0500

    tweaks what data is sent when user connects to room

--- app.js
+++ app.js
@@ -20,9 +20,6 @@ io.sockets.on('connection', function (socket) {
 		if (data.user.email) {
 			// update user info
 			socket.user.avatar = gravatar.url(data.user.email, {s: '60', r: 'x', d: '404'});
-			socket.emit('user_info', {
-				user: socket.user
-			});
 		}
 		console.log('user logged in: %s', socket.user.username);
 	});
@@ -40,13 +37,13 @@ io.sockets.on('connection', function (socket) {
 		// TODO: validate that user is allowed to join room
 		socket.room = data.room;
 
-		// broadcast to other users
-		socket.broadcast.to(data.room).emit('user_joined', {
-			user: socket.user
-		});
-
 		// join the room
 		socket.join(data.room);
+
+		// sent to all users in room
+		io.sockets.to(data.room).emit('user_joined', {
+			user: socket.user
+		});
 
 		// send all current users in room
 		var users = io.sockets.clients(data.room).map(function(c) {return c.user;});
@@ -77,10 +74,14 @@ io.sockets.on('connection', function (socket) {
 	});
 
 	socket.leave_room = function() {
-		socket.broadcast.to(socket.room).emit('user_left', {
+		// send to all users in room
+		io.sockets.to(socket.room).emit('user_left', {
 			user: socket.user
 		});
+
+		// leave room
 		socket.leave(socket.room);
+
 		console.log("%s left room %s", socket.user.username, socket.room);
 		delete socket.room;
 	};
